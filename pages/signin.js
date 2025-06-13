1a2c4a3e04453b21826960b6510e5a8686c2197b
HayLuiza <hlhabade@gmail.com>
2025-06-13 12:10:38 -0300

    adicionando estrutura inicial e adicionando Template Default, Header, User Dashboard

---
+++ pages/signin.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  Link,
+  Paper,
+  Grid,
+  Typography,
+  Box,
+} from '@mui/material'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+
+export default function SignInSide() {
+  return (
+    <Grid container component="main" sx={{ height: '100vh' }}>
+      <CssBaseline />
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={7}
+        sx={{
+          backgroundImage: 'url(https://source.unsplash.com/random)',
+          backgroundRepeat: 'no-repeat',
+          backgroundColor: '#ccc',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Box
+          sx={{
+            my: 8,
+            mx: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Acesse a sua conta
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="E-mail"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Senha"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Entrar
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Esqueceu a senha?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Não tem uma conta? Cadastre-se"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Grid>
+    </Grid>
+  )
+}
---
+++ pages/user/dashboard.js
@@ -0,0 +1,149 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Typography
+} from '@mui/material'
+
+import TemplateDefault from '../../src/templates/Default'
+
+export default function Home() {
+  return (
+    <TemplateDefault>
+      <Container maxWidth="sm" sx={{ py: 6 }}>
+        <Typography component="h1" variant="h2" align="center">
+          Meus Anúncios
+        </Typography>
+
+        <Button variant="contained"
+          sx={{
+            margin: '30px auto',
+            display: 'block', 
+          }}
+        >
+          Publicar novo anúncio
+        </Button>
+      </Container>
+      <Container maxWidth="md">
+        <Grid container spacing={4}>
+          <Grid sx={{ xs: 12, sm: 6, md: 4 }}>
+            <Card>
+              <CardMedia
+                sx={{
+                  paddingTop: '56%',
+                }}
+                image={'https://source.unsplash.com/random'}
+                title="Título da imagem"
+              />
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                  Produto W
+                </Typography>
+                <Typography>
+                  R$ 80,00
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Editar
+                </Button>
+                <Button size="small" color="primary">
+                  Remover
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+
+          <Grid sx={{ xs: 12, sm: 6, md: 4 }}>
+            <Card>
+              <CardMedia
+                sx={{
+                  paddingTop: '56%',
+                }}
+                image={'https://source.unsplash.com/random'}
+                title="Título da imagem"
+              />
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                  Produto X
+                </Typography>
+                <Typography>
+                  R$ 60,00
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Editar
+                </Button>
+                <Button size="small" color="primary">
+                  Remover
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          
+          <Grid sx={{ xs: 12, sm: 6, md: 4 }}>
+            <Card>
+              <CardMedia
+                sx={{
+                  paddingTop: '56%',
+                }}
+                image={'https://source.unsplash.com/random'}
+                title="Título da imagem"
+              />
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                  Produto Y
+                </Typography>
+                <Typography>
+                  R$ 100,00
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Editar
+                </Button>
+                <Button size="small" color="primary">
+                  Remover
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          
+          
+          <Grid sx={{ xs: 12, sm: 6, md: 4 }}>
+            <Card>
+              <CardMedia
+                sx={{
+                  paddingTop: '56%',
+                }}
+                image={'https://source.unsplash.com/random'}
+                title="Título da imagem"
+              />
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                  Produto Z
+                </Typography>
+                <Typography>
+                  R$ 90,00
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Editar
+                </Button>
+                <Button size="small" color="primary">
+                  Remover
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
+    </TemplateDefault>
+  )
+}
