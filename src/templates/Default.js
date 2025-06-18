645c6c0dda24e681168d398365c321d62bcb14b5
HayLuiza <hlhabade@gmail.com>
2025-06-18 11:35:11 -0300

    adicionando home deslogada

--- src/templates/Default.js
+++ src/templates/Default.js
@@ -1,12 +1,13 @@
+import { Box } from '@mui/material'
 import Header from '../components/Header'
 
-const Default = ({ children }) => {
+const Default = ({ children, sx }) => {
   return (
-    <>
+    <Box sx={{ minHeight: '100vh', ...sx }}>
       <Header />
-       {children}
+      {children}
       <footer>FOOTER</footer>
-    </>
+    </Box>
   )
 }
 
