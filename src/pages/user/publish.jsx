1e3a398f112b84565e001fae961f5af3004d0fc6
HayLuiza <hlhabade@gmail.com>
2025-07-04 12:59:19 -0300

    validacao do campo Categoria

--- src/pages/user/publish.jsx
+++ src/pages/user/publish.jsx
@@ -15,6 +15,8 @@ import {
   InputLabel,
   OutlinedInput,
   InputAdornment,
+  MenuItem,
+  FormHelperText,
 } from '@mui/material'
 
 import { useTheme } from '@mui/material/styles'
@@ -27,7 +29,9 @@ const validationSchema = object({
   title: string()
     .min(6, 'Escreva um título maior')
     .max(100, 'Título muito grande')
-    .required('Campo obrigatório*')
+    .required('Campo obrigatório*'),
+
+  category: string().required('Campo obrigatório*')
 })
 
 const Publish = () => {
@@ -53,7 +57,8 @@ const Publish = () => {
     <TemplateDefault>
       <Formik
         initialValues={{
-          title: ''
+          title: '',
+          category: '',
         }}
         validationSchema={validationSchema}
       >
@@ -96,30 +101,33 @@ const Publish = () => {
                     <Typography component="h6" variant="h6" color="textPrimary">
                       Categoria
                     </Typography>
-                    <Select
-                      native
-                      value=""
-                      fullWidth
-                      onChange={() => {}}
-                      inputProps={{ name: 'age' }}
-                    >
-                      <option value="">Selecione</option>
-                      <option value={1}>Bebê e Criança</option>
-                      <option value={2}>Agricultura</option>
-                      <option value={3}>Moda</option>
-                      <option value={4}>Carros, Motos e Barcos</option>
-                      <option value={5}>Serviços</option>
-                      <option value={6}>Lazer</option>
-                      <option value={7}>Animais</option>
-                      <option value={8}>Móveis, Casa e Jardim</option>
-                      <option value={9}>Imóveis</option>
-                      <option value={10}>Equipamentos e Ferramentas</option>
-                      <option value={11}>Celulares e Tablets</option>
-                      <option value={12}>Esporte</option>
-                      <option value={13}>Tecnologia</option>
-                      <option value={14}>Emprego</option>
-                      <option value={15}>Outros</option>
-                    </Select>
+                    <FormControl error={errors.category} fullWidth>
+                      <Select
+                        name="category"
+                        value={values.category}
+                        fullWidth
+                        onChange={handleChange}
+                      >
+                        <MenuItem value="Bebê e Criança">Bebê e Criança</MenuItem>
+                        <MenuItem value="Agricultura">Agricultura</MenuItem>
+                        <MenuItem value="Moda">Moda</MenuItem>
+                        <MenuItem value="Carros, Motos e Barcos">Carros, Motos e Barcos</MenuItem>
+                        <MenuItem value="Serviços">Serviços</MenuItem>
+                        <MenuItem value="Lazer">Lazer</MenuItem>
+                        <MenuItem value="Animais">Animais</MenuItem>
+                        <MenuItem value="Móveis, Casa e Jardim">Móveis, Casa e Jardim</MenuItem>
+                        <MenuItem value="Imóveis">Imóveis</MenuItem>
+                        <MenuItem value="Equipamentos e Ferramentas">Equipamentos e Ferramentas</MenuItem>
+                        <MenuItem value="Celulares e Tablets">Celulares e Tablets</MenuItem>
+                        <MenuItem value="Esporte">Esporte</MenuItem>
+                        <MenuItem value="Tecnologia">Tecnologia</MenuItem>
+                        <MenuItem value="Emprego">Emprego</MenuItem>
+                        <MenuItem value="Outros">Outros</MenuItem>
+                      </Select>
+                      <FormHelperText>
+                        { errors.category }
+                      </FormHelperText>
+                    </FormControl>
                   </Box>
                 </Container>
 
